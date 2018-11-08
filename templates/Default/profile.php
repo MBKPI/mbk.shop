d98aee7a9f3db7190cb21aaf7d91bdd78c546de5
Kostya Karaman <kosty.karaman4@gmail.com>
2018-11-08 09:22:40 +0200

    Добавил модальное и форму редактирования профиля, и подсказку при наведении

--- templates/Default/profile.php
+++ templates/Default/profile.php
@@ -22,9 +22,31 @@
         <p>Мои Объвления</p>
 
         <div id="username" class="ml-auto">
-          <?=$this->user['name']?> <?=$this->user['surname']?><span><a href="/logout">(Выйти)</a></span>
+          <a id="edit" data-toggle="modal" data-target="#exampleModal" title = "Нажмите для редактирования профиля"><?=$this->user['name']?> <?=$this->user['surname']?></a><span><a href="/logout">(Выйти)</a></span>
         </div>
-
+        <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Редактирование аккаунта</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form class="editacount" action="index.html" method="post">
+          <input type="text" name="editname" placeholder="Измените Имя">
+          <input type="text" name="editpassword" placeholder="Измените пароль">
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+        <button type="button" class="btn btn-primary">Сохранить изменения</button>
+      </div>
+    </div>
+  </div>
+</div>
       </div>
 
 
