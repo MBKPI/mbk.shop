1d7a7f343eac3e795dfa4f80aa5ddc5877523d0b
redyxred <emaxim.news@gmail.com>
2018-11-09 08:45:57 +0200

    DB

--- mbk_shop_user.sql
+++ mbk_shop_user.sql
@@ -1,10 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.7.3
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 06 2018 г., 11:47
--- Версия сервера: 5.6.38-log
+-- Время создания: Ноя 09 2018 г., 09:45
+-- Версия сервера: 5.6.37-log
 -- Версия PHP: 5.5.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -21,19 +21,6 @@ SET time_zone = "+00:00";
 --
 -- База данных: `mbk_shop_user`
 --
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `advert`
---
-
-CREATE TABLE `advert` (
-  `id` int(11) NOT NULL,
-  `captiopn` varchar(255) NOT NULL,
-  `price` varchar(75) NOT NULL,
-  `text` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -55,32 +42,8 @@ CREATE TABLE `lots` (
 --
 
 INSERT INTO `lots` (`lot_id`, `user_id`, `title`, `about`, `price`, `image`) VALUES
-(1, 1, 'Айфон 5с', 'Крутой телефон', 2000, 'no_image.jpg'),
-(2, 1, 'IPhone XS Max 256 GB Silver', 'aaaaaanj jk gvj cyi ctyiv ciu vuo<?=$this->path?>aaaaaanj jk gvj cyi ctyiv ciu vuo<?=$this->path?>aaaaaanj jk gvj cyi ctyiv ciu vuo<?=$this->path?>aaaaaanj jk gvj cyi ctyiv ciu vuo<?=$this->path?>aaaaaanj jk gvj cyi ctyiv ciu vuo<?=$this->path?>aaaaaanj jk gvj cyi ctyiv ciu vuo<?=$this->path?>aaaaaanj jk gvj cyi ctyiv ciu vuo<?=$this->path?>', 14000, 'no_image.jpg'),
-(3, 2, 'JJBasdkjasdl', 'anbjsdmmzvz;lsdlal;', 12111, 'no_image.jpg'),
-(4, 1, 'Samsung J7 (2018)', 'Лучший телефон на рынке телефонав', 10999, 'no_image.jpg');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `phone` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `phone`) VALUES
-(13, 'Maxim', 'emaxim.news@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '380636015336'),
-(14, 'Kostya Karaman', 'kostya.karaman4@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '+555522145');
+(4, 1, 'Samsung J7 (2018)', 'Лучший телефон на рынке телефонав', 10999, 'no_image.jpg'),
+(6, 1, 'asdasdasd', 'lkasjdkfllasd', 92881, 'no_image.jpg');
 
 -- --------------------------------------------------------
 
@@ -93,26 +56,22 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL
+  `surname` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `admin` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`user_id`, `email`, `password`, `name`, `surname`) VALUES
-(1, 'emaxim.news@gmail.com', '926d61bbf37d585b3db5354f90288b69', 'Maxim', 'Shibkov'),
-(2, 'demo@admin.ru', '926d61bbf37d585b3db5354f90288b69', 'Demo', 'User');
+INSERT INTO `users` (`user_id`, `email`, `password`, `name`, `surname`, `phone`, `admin`) VALUES
+(1, 'emaxim.news@gmail.com', '926d61bbf37d585b3db5354f90288b69', 'Maxim', 'Shibkov', '', 1),
+(4, 'karaman91@gmail.com', '926d61bbf37d585b3db5354f90288b69', 'Demo', 'User', '380636015336', 1);
 
 --
 -- Индексы сохранённых таблиц
 --
-
---
--- Индексы таблицы `advert`
---
-ALTER TABLE `advert`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `lots`
@@ -120,12 +79,6 @@ ALTER TABLE `advert`
 ALTER TABLE `lots`
   ADD PRIMARY KEY (`lot_id`),
   ADD KEY `user_id` (`user_id`);
-
---
--- Индексы таблицы `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `users`
@@ -138,29 +91,15 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT для таблицы `advert`
---
-ALTER TABLE `advert`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT для таблицы `lots`
 --
 ALTER TABLE `lots`
-  MODIFY `lot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT для таблицы `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
+  MODIFY `lot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
---
+++ templates/Default/index-new.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?=$this->path?>/css/style.css">
+  <title><?=$this->title?></title>
+</head>
+<body>
+
+  <div class="container">
+
+    <div class="row justify-content-between">
+      <div class="col-md-5 mt-5">
+        <div class="logotype"><a href="/">MBKshop</a></div>
+      </div>
+      <div class="col-md-5 mt-5">
+        <div class="profile text-right"><a href="/profile"><i class="far fa-user-circle "></i> Мой профиль</a></div>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-md-6 mt-5 text-center add-button">
+        <a href="/add" class="btn btn-primary btn-lg btn-block"><i class="fas fa-plus"></i> Добавить объявление</a>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-md-6 mt-5 text-center">
+        <div class="input-group input-group-lg">
+          <input type="search" class="form-control" placeholder="Что требуется найти?" aria-label="Что требуется найти?" aria-describedby="button-addon2">
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fas fa-search"></i> Поиск</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col mt-5">
+        <? for($i = 0; $i < count($this->lots); $i++): ?>
+        <div class="card mb-3">
+          <img class="card-img-top" src="/uploads/lots/no_image.jpg" width="1149px" height="180px" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title"><?=$this->lots[$i]['title']?> <p class="float-right text-muted">Цена: <b><?=$this->lots[$i]['price']?> грн.</b></p></h5>
+            <p class="card-text"><?=$this->lots[$i]['about']?></p>
+            <p class="card-text"><small class="text-muted">Опубликовано 3 минуты назад</small></p>
+          </div>
+        </div>
+        <? endfor; ?>
+      </div>
+    </div>
+
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
