6d5b5d00ad202f130dd22339a77eb49f76ebb8e0
redyxred <emaxim.news@gmail.com>
2018-11-19 17:48:29 +0200

    Исправлен мелький недочёт.

--- mbk_shop_user.sql
+++ mbk_shop_user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 12 2018 г., 11:01
+-- Время создания: Ноя 19 2018 г., 18:47
 -- Версия сервера: 5.6.37-log
 -- Версия PHP: 5.5.38
 
@@ -41,7 +41,10 @@ CREATE TABLE `categories` (
 
 INSERT INTO `categories` (`category_id`, `name`, `name_eng`, `parent_category`) VALUES
 (7, 'Телефоны', 'telephones', NULL),
-(8, 'Автомобили', 'auto', NULL);
+(8, 'Автомобили', 'auto', NULL),
+(9, 'Часы', 'watch', NULL),
+(10, 'Смарт-часы', 'smartwatch', 9),
+(11, 'Игровые аккаунты', 'game-accounts', NULL);
 
 -- --------------------------------------------------------
 
@@ -76,6 +79,13 @@ CREATE TABLE `favourites` (
   `date` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `favourites`
+--
+
+INSERT INTO `favourites` (`favourites_id`, `lot_id`, `user_id`, `date`) VALUES
+(7, 19, 6, '15-11-2018, в 15:43');
+
 -- --------------------------------------------------------
 
 --
@@ -92,6 +102,7 @@ CREATE TABLE `lots` (
   `image` varchar(255) NOT NULL DEFAULT 'no_image.jpg',
   `state` varchar(255) NOT NULL,
   `category_id` int(11) DEFAULT NULL,
+  `views` int(11) NOT NULL DEFAULT '0',
   `date` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -99,8 +110,13 @@ CREATE TABLE `lots` (
 -- Дамп данных таблицы `lots`
 --
 
-INSERT INTO `lots` (`lot_id`, `user_id`, `title`, `about`, `price`, `currency_id`, `image`, `state`, `category_id`, `date`) VALUES
-(13, 4, 'Кахон', 'Продаю кахон. Отличное состояние.Отличный звук.', 1800, 1, 'image (1).jpg', 'Б/у', 8, 'фывфыв');
+INSERT INTO `lots` (`lot_id`, `user_id`, `title`, `about`, `price`, `currency_id`, `image`, `state`, `category_id`, `views`, `date`) VALUES
+(13, 4, 'Кахон', 'Продаю кахон. Отличное состояние.Отличный звук.', 1800, 1, 'image (1).jpg', 'Б/у', 8, 7, 'фывфыв'),
+(15, 4, 'Apple Watch Series 3 (42mm) Space Gray', 'Умные подсказки про физическую активность. Усовершенствованное приложение «Пульс». Любимые плейлисты прямо на запястье. Встроенный высотомер. И более мощный процессор, благодаря которому Siri на часах может говорить. Представляем Apple Watch Series 3. У них как никогда много полезных функций, которые мотивируют на новые достижения, помогают быть активнее и позволяют оставаться на связи.', 8999, 1, '4d67350743cad6eaaa7426854502d8b9.png', 'Новое', 10, 67, '12-11-2018, в 11:19'),
+(16, 4, 'Apple Watch Series 3 (42mm) White', 'Умные подсказки про физическую активность. Усовершенствованное приложение «Пульс». Любимые плейлисты прямо на запястье. Встроенный высотомер. И более мощный процессор, благодаря которому Siri на часах может говорить. Представляем Apple Watch Series 3. У них как никогда много полезных функций, которые мотивируют на новые достижения, помогают быть активнее и позволяют оставаться на связи.', 8999, 1, 'd68718f18f58607d339783da9d180116.png', 'Новое', 10, 14, '12-11-2018, в 13:03'),
+(17, 4, 'Продаю сайт-визитку', 'БлаблабалабалалБлаблабалабалалБлаблабалабалалБлаблабалабалал', 1000, 1, 'Background.png', 'Новое', 7, 16, '13-11-2018, в 11:21'),
+(18, 6, 'ПРадаю ебаную капейку', 'Катаеца норм', 9872, 1, 'maxresdefault.jpg', 'Б/у', 8, 5, '14-11-2018, в 14:54'),
+(19, 6, 'Продаю iPhone 6s 64Gb Silver', 'Айклауд чистый, телефон в идеально состоянии, звоните узнавайте!', 300, 2, 'p4375_92909_chehol_elago_s5_slim_fit_extreme_hot_red_for_iphone_5 (1).png', 'Б/у', 7, 2, '15-11-2018, в 15:43');
 
 -- --------------------------------------------------------
 
@@ -124,7 +140,27 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `email`, `password`, `name`, `surname`, `phone`, `admin`) VALUES
 (4, 'karaman91@gmail.com', '926d61bbf37d585b3db5354f90288b69', 'Костя', 'Караман', '380636015336', 1),
-(6, 'emaxim.news@gmail.com', '926d61bbf37d585b3db5354f90288b69', 'Максим', 'Шибков', '636015336', 0);
+(6, 'emaxim.news@gmail.com', '926d61bbf37d585b3db5354f90288b69', 'Максим', 'Шибков', '636015336', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `views`
+--
+
+CREATE TABLE `views` (
+  `view_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `lot_id` int(11) DEFAULT NULL,
+  `date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `views`
+--
+
+INSERT INTO `views` (`view_id`, `user_id`, `lot_id`, `date`) VALUES
+(1, 6, 16, '');
 
 --
 -- Индексы сохранённых таблиц
@@ -167,6 +203,14 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
+-- Индексы таблицы `views`
+--
+ALTER TABLE `views`
+  ADD PRIMARY KEY (`view_id`),
+  ADD KEY `user_id` (`user_id`,`lot_id`),
+  ADD KEY `lots_12` (`lot_id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -174,7 +218,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT для таблицы `currency`
 --
@@ -184,17 +228,22 @@ ALTER TABLE `currency`
 -- AUTO_INCREMENT для таблицы `favourites`
 --
 ALTER TABLE `favourites`
-  MODIFY `favourites_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `favourites_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT для таблицы `lots`
 --
 ALTER TABLE `lots`
-  MODIFY `lot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `lot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT для таблицы `views`
+--
+ALTER TABLE `views`
+  MODIFY `view_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
@@ -219,6 +268,13 @@ ALTER TABLE `lots`
   ADD CONSTRAINT `categories_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `currency_1` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`currency_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `users_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `views`
+--
+ALTER TABLE `views`
+  ADD CONSTRAINT `lots_12` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`lot_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_123` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
